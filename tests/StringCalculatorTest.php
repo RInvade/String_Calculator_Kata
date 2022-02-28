9d02f60920edba0bb719b378217217c400e77ff1
Youssef <benbelkheir.127607@e.unavarra.es>
2022-02-28 08:27:01 +0100

    problem space inizialitation

---
+++ tests/StringCalculatorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Deg540\PHPTestingBoilerplate\Test;
+
+use Deg540\PHPTestingBoilerplate\StringCalculator;
+use PHPUnit\Framework\TestCase;
+
+final class StringCalculatorTest extends TestCase
+{
+
+}
